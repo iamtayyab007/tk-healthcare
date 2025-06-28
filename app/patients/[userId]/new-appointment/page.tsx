6ff03bb5621b77702866ff9f251a8d38cde0b1bf
iamtayyab007 <tayyabm336@gmail.com>
2025-06-29 00:19:27 +0500

    appointment form in progress

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
 import React from "react";
 
 function NewAppointment() {
-  return <div>THis is the appointment page</div>;
+  return (
+    <div className="h-screen w-[80%]">
+      <section className="flex flex-col">
+        <div className="m-11 ">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={150}
+            height={150}
+            alt="logo"
+          />
+        </div>
+        <div className="m-11 flex flex-col gap-3">
+          <h1 className="font-bold text-xl">Hey there 🖐️ </h1>
+          <p className="text-sm">Request a new appointment in 10 seconds</p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default NewAppointment;
